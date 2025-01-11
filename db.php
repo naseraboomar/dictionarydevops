cf5f13d4a5c0dd6bfcbabe047f4509c166340d03
naseraboomar <aboomarn871@gmail.com>
2025-01-11 09:42:47 +0000

    Added db.php and index.php

--- db.php
+++ db.php
@@ -1,13 +1,13 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "password"; // أدخل كلمة المرور التي استخدمتها للدخول إلى MySQL
+$password = "password"; // أ
 $dbname = "dictionary";
 
-// إنشاء الاتصال
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// التحقق من الاتصال
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
