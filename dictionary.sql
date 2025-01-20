124713e72be7ff212ad692e41df8edb503f98db2
naseraboomar <aboomarn871@gmail.com>
2025-01-20 12:57:51 +0000

    ff

--- dictionary.sql
+++ dictionary.sql
@@ -31,7 +31,7 @@ CREATE TABLE `words` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `words`
+-- Dumping data for tabjle `words`
 --
 
 LOCK TABLES `words` WRITE;
